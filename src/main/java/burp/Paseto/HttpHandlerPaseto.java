bdeef141c76d4c7aa6b4e87d22f3fdd03c3ca1cb
Philipp Röder <Philipp.Roeder@ibm.com>
2025-08-12 15:30:48 +0200

    mutiple tokens and detection of correct request

--- src/main/java/burp/Paseto/HttpHandlerPaseto.java
+++ src/main/java/burp/Paseto/HttpHandlerPaseto.java
@@ -6,11 +6,15 @@ import burp.api.montoya.http.handler.*;
 import burp.api.montoya.http.message.requests.HttpRequest;
 import burp.api.montoya.ui.settings.SettingsPanelWithData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HttpHandlerPaseto implements HttpHandler {
     private boolean dirty;
-    private HttpRequest pasetoRequest;
-    private int hash_id;
+    private List<HttpRequest> pasetoRequest= new ArrayList<>();;
+    //private String hash_id;
+    private List<String> hash_id = new ArrayList<>();
     private boolean markRequests;
     private SettingsPanelWithData settings;
 
@@ -25,13 +29,17 @@ public class HttpHandlerPaseto implements HttpHandler {
     @Override
     public RequestToBeSentAction handleHttpRequestToBeSent(HttpRequestToBeSent httpRequestToBeSent) {
         // Example header injection kept from the original sample
+        String id = httpRequestToBeSent.headerValue("X-Paseto-Edit-Id");
         Annotations annotations = Annotations.annotations(null, null);
         HttpRequest request=httpRequestToBeSent;
-        if(dirty){
-            request=this.pasetoRequest;
+        if(dirty&&(this.hash_id.contains(id))){
+            int request_index=this.hash_id.indexOf(id);
+            request=this.pasetoRequest.get(request_index).withRemovedHeader("X-Paseto-Edit-Id");
             if(markRequests()){
                 annotations = Annotations.annotations(null, HighlightColor.GREEN);
             }
+            this.hash_id.remove(request_index);
+            this.pasetoRequest.remove(request_index);
         }
         // Continue with the (possibly) modified request
 
@@ -49,12 +57,12 @@ public class HttpHandlerPaseto implements HttpHandler {
         this.dirty=val;
     }
     void setPassetoRequest(HttpRequest request){
-        this.pasetoRequest=request;
+        this.pasetoRequest.add(request);
     }
 
 
-    public void setId(int hash_id) {
-        this.hash_id = hash_id;
+    public void setId(String hash_id) {
+        this.hash_id.add(hash_id);
     }
 
 
